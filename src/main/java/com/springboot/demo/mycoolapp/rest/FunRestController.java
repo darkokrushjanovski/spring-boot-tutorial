edcb59becb0bd5c43793f7ae99fb09ffe9ac2fd3
Darko Krushjanovski <45966349+darkokrushjanovski@users.noreply.github.com>
2023-07-27 17:46:52 +0200

    dependency injection tutorial

--- src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,25 +1,22 @@
 package com.springboot.demo.mycoolapp.rest;
 
+import com.springboot.demo.mycoolapp.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that returns "Hello world"
+private Coach myCoach;
 
-
-    @GetMapping("/")
-    public String sayHello(){
-        return "Hello world";
+    @Autowired
+    public FunRestController(Coach myCoach) {
+        this.myCoach = myCoach;
     }
 
-    @GetMapping("test")
-    public String testFunction(){
-        String a = "test1";
-        String b = "test2";
-        String c = a.concat(b);
-
-        return c;
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
     }
 
 }
