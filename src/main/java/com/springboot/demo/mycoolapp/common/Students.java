0d0ebd321def5900c371fb41fc1f6ecc30cb8bfa
Darko Krushjanovski <45966349+darkokrushjanovski@users.noreply.github.com>
2023-07-31 18:33:43 +0200

    Added exceptions to student controller

--- src/main/java/com/springboot/demo/mycoolapp/common/Students.java
+++ src/main/java/com/springboot/demo/mycoolapp/common/Students.java
@@ -5,6 +5,10 @@ public class Students {
     private String firstName;
     private String lastName;
 
+    public Students(){
+
+    }
+
     public Students(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
--- src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,11 +1,15 @@
 package com.springboot.demo.mycoolapp.rest;
 
+import com.springboot.demo.mycoolapp.common.StudentErrorResponse;
+import com.springboot.demo.mycoolapp.common.StudentNotFoundException;
 import com.springboot.demo.mycoolapp.common.Students;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,14 +18,55 @@ import java.util.List;
 @RequestMapping("/api")
 public class FunRestController {
 
+    private List<Students> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+
+        students.add(new Students("Johny", "Cash"));
+        students.add(new Students("Terry", "Brown"));
+        students.add(new Students("Jack", "Black"));
+    }
+
 
     @GetMapping("/students")
-    public List<Students> getDailyWorkout() {
-        List<Students> students = new ArrayList<>();
-
-        students.add(new Students("Johny","Cash"));
-        students.add(new Students("Terry","Brown"));
-        students.add(new Students("Jack","Black"));
+    public List<Students> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentsId}")
+    public Students getStudent(@PathVariable int studentsId) {
+
+
+        if ((studentsId >= students.size()) || studentsId < 0) {
+            throw new StudentNotFoundException(("Student id not found: " + studentsId));
+        }
+        Students student = students.get(studentsId);
+        return student;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    ;
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
+
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exception.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
